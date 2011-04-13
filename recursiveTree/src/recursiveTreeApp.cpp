558f15c1302418e6d71527108604b66b2687b7d9
Nate Murray <nate@natemurray.com>
2011-04-13 16:30:06 -0700

    added paramaters

--- recursiveTree/src/recursiveTreeApp.cpp
+++ recursiveTree/src/recursiveTreeApp.cpp
@@ -1,6 +1,8 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
 #include "cinder/Rand.h"
+#include "cinder/params/Params.h"
+
 
 using namespace ci;
 using namespace ci::app;
@@ -15,15 +17,46 @@ class recursiveTreeApp : public AppBasic {
 	void branch( int depth, float width );
 
     int mReset;
+	params::InterfaceGl		mParams;
+
+    float mStartingWidth;
+    int mMaxDepth;
+    int mSegmentLengthMin;
+    int mSegmentLengthMax;
+    float mBranchRotationMin;
+    float mBranchRotationMax;
+    float mProbBranch;
+    float mScaleReduction;
+
 };
 
 void recursiveTreeApp::setup()
 {
   Rand::randomize();
   gl::enableAlphaBlending();
-  gl::clear( Color( 0.898f, 0.831f, 0.604f ) );
+  gl::clear( Color( 1.0, 1.0, 1.0) );
   glEnable(GL_LINE_SMOOTH);
   mReset = 0;
+
+  mStartingWidth = 11.0;
+  mMaxDepth = 12;
+  mSegmentLengthMin = 4;
+  mSegmentLengthMax = 14;
+  mBranchRotationMin = -13.0f;
+  mBranchRotationMax = 11.0f;
+  mProbBranch = 0.7f;
+  mScaleReduction = 0.7f;
+
+  // Setup the parameters
+  mParams = params::InterfaceGl( "Parameters", Vec2i( 200, 400 ) );
+  mParams.addParam( "Starting Width", &mStartingWidth, "min=1 max=20 step=1" );
+  mParams.addParam( "Max Depth", &mMaxDepth, "min=1 max=20 step=1" );
+  mParams.addParam( "Seg Length (min)", &mSegmentLengthMin, "min=1 max=30 step=1" );
+  mParams.addParam( "Seg Length (max)", &mSegmentLengthMax, "min=1 max=30 step=1" );
+  mParams.addParam( "Branch Rotation (min)", &mBranchRotationMin, "min=-360 max=360 step=1" );
+  mParams.addParam( "Branch Rotation (max)", &mBranchRotationMax, "min=-360 max=360 step=1" );
+  mParams.addParam( "Prob Branch", &mProbBranch, "min=0.0 max=1.0 step=0.05" );
+  mParams.addParam( "Scale Reduction", &mScaleReduction, "min=0.0 max=1.0 step=0.05" );
 }
 
 void recursiveTreeApp::mouseDown( MouseEvent event )
@@ -40,97 +73,54 @@ void recursiveTreeApp::update()
 
 void recursiveTreeApp::draw()
 {
-  gl::color( ColorA( 0.898f, 0.831f, 0.604f, 0.01f ) ); 
-  gl::drawSolidRect( getWindowBounds() );
+  // gl::color( ColorA( 1.0f, 1.0f, 1.0f, 0.01f ) ); 
+  // gl::drawSolidRect( getWindowBounds() );
 
   if(mReset == 0) {
     gl::pushMatrices();
     gl::translate( Vec2f( getWindowWidth()/2, getWindowHeight() ) );
-    // gl::clear( Color( 0.898f, 0.831f, 0.604f ) );
-    branch(0, 20.0);
+    gl::clear( Color( 1.0f, 1.0f, 1.0f ) );
+    gl::rotate(Rand::randFloat(-3, 3)); 
+    branch(0, mStartingWidth);
     gl::popMatrices();
     mReset = 1;
   }
+	
+  params::InterfaceGl::draw();
+
 }
 
 void recursiveTreeApp::branch(int depth, float width) 
 {
   gl::color( Color( 0.1f, 0.1f, 0.1f ) );
   glLineWidth( width );
-  printf("depth: %d width:%f\n", depth, width);
+  // printf("depth: %d width:%f\n", depth, width);
 
-  if (depth < 10) {
-    gl::drawLine( Vec2f(0, 0), Vec2f(0, -getWindowHeight()/3) );
+  if (depth < mMaxDepth) {
 
-    gl::pushMatrices();
-    {
-      gl::translate(Vec2f(0, -getWindowHeight()/5));
-      gl::rotate(Rand::randFloat(-45.0, 45.0)); 
-      gl::scale(Vec3f( 0.7f, 0.7f, 0.7f ));
-      branch(depth + 1, width * 0.7);
+    float segmentLength = Rand::randFloat(mSegmentLengthMin, mSegmentLengthMax);
+    gl::drawLine( Vec2f(0, 0), Vec2f(0, -getWindowHeight()/segmentLength) );
+    gl::translate(Vec2f(0, -getWindowHeight()/segmentLength));
+    gl::rotate(Rand::randFloat(mBranchRotationMin, mBranchRotationMax)); 
+
+    if (Rand::randFloat(1.0) < mProbBranch) { // branching
+      gl::rotate(toDegrees(0.3)); 
+      gl::scale(Vec3f( mScaleReduction, mScaleReduction, mScaleReduction ));
+
+      gl::pushMatrices();
+      branch(depth + 1, width*mScaleReduction) ;
+      gl::popMatrices();
+
+      gl::rotate(toDegrees(-0.6)); 
+      gl::pushMatrices();
+      branch(depth + 1, width*mScaleReduction) ;
+      gl::popMatrices();
+    } else { // continue
+      branch(depth, width);
     }
-    gl::popMatrices();
-
-    gl::pushMatrices();
-    {
-      gl::translate(Vec2f(0, -getWindowHeight()/3));
-      gl::rotate(Rand::randFloat(-45.0, 45.0)); 
-      gl::scale(Vec3f( 0.7f, 0.7f, 0.7f ));
-      branch(depth + 1, width * 0.7);
-    }
-    gl::popMatrices();
 
   }
 }
 
 CINDER_APP_BASIC( recursiveTreeApp, RendererGl )
 
-
-// A simple recursive tree
-// (c) Alasdair Turner 2007
-/* 
-
-void setup()
-{
-  size(400,400);
-  smooth();
-  noLoop();
-}
- 
-void draw()
-{
-  background(204);  
-  strokeWeight(10);
-  translate(width/2,height);
-  branch(0);
-}
- 
-void branch(int depth)
-{
-    if (depth < 10) {
-      line(0,0,0,-height/3);
-      pushMatrix();
-      {
-        translate(0,-height/5);
-        rotate(random(-PI/4,PI/4));
-        scale(0.7);
-        branch(depth + 1);
-      }
-      popMatrix();
-      pushMatrix();
-      {
-        translate(0,-height/3);
-        rotate(random(-PI/4,PI/4));
-        scale(0.7);
-        branch(depth + 1);
-      }
-      popMatrix();
-    }
-}
- 
-void mouseClicked()
-{
-  redraw();
-}
-
-*/
